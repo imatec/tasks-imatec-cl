b4d385d7600d7ba3b00269c0c117f85548a4c167
Jorge Alvarez Solovioff <jorge@imatec.cl>
2012-11-30 16:24:41 -0300

    as

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 		}	
     });
 	$(document).on('keypress', '#newTask', function(e){
-		console.log('pressed');
+		console.log('pressed ' + e.which);
 		if(e.which == 13 && $.trim($(this).text()) != ''){
 			console.log($(this).val());
 		}
